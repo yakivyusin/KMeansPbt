fbf928779c5c8be079dabdeac06e708047c1f869
Yakiv Yusin <yusin.yakiv@gmail.com>
2020-09-16 17:25:36 +0300

    Added K-Means Clusterer

---
+++ KMeansPbt/KMeans.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+using System.Linq;
+
+namespace KMeansPbt
+{
+    public class KMeans
+    {
+        private readonly KMeansTrainer.InitializationAlgorithm _initializationAlgorithm;
+
+        public KMeans(KMeansTrainer.InitializationAlgorithm initializationAlgorithm)
+        {
+            _initializationAlgorithm = initializationAlgorithm;
+        }
+
+        public Data[] Clusterize(Data[] data, uint k)
+        {
+            var mlContext = new MLContext();
+            var schemaDef = SchemaDefinition.Create(typeof(Data));
+            schemaDef["Features"].ColumnType = new VectorDataViewType(NumberDataViewType.Single, data[0].Features.Length);
+
+            var trainingData = mlContext.Data.LoadFromEnumerable(data, schemaDef);
+            var pipeline = mlContext.Clustering.Trainers.KMeans(new KMeansTrainer.Options
+            {
+                FeatureColumnName = "Features",
+                InitializationAlgorithm = _initializationAlgorithm,
+                NumberOfClusters = (int)k
+            });
+
+            var model = pipeline.Fit(trainingData);
+            var outputData = model.Transform(trainingData);
+
+            return mlContext.Data
+                .CreateEnumerable<Data>(outputData, reuseRowObject: false)
+                .ToArray();
+        }
+    }
+
+    public class Data
+    {
+        public float[] Features { get; set; }
+        [ColumnName("PredictedLabel")]
+        public uint PredictedClusterId;
+        [ColumnName("Score")]
+        public float[] Distances;
+    }
+}
